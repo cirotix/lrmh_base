c1cac87ad53b34baeb434283a80dd1e19215f21e
Damien Cirotteau <d@cirotteau.info>
2017-12-23 10:18:33 +0100

    chore(settings): database setting

--- lrmh_base/settings/default.py
+++ lrmh_base/settings/default.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,7 @@ WSGI_APPLICATION = 'lrmh_base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        **dj_database_url.parse(os.environ.get('APP_DATABASE'))
     }
 }
 
